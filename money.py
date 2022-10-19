c4e46485d97e90438b01dae14c4f64fa0605baaf
Dimitrios Starantzis <58553332+jimwheaty@users.noreply.github.com>
2022-10-19 12:32:46 +0300

    Update money.py

--- money.py
+++ money.py
@@ -9,8 +9,6 @@ from math import floor
 # Οι ασφαλιστικές κατηγορίες για ελεύθερους επαγγελματίες είναι: 210 252 302 363 435 566 ευρώ ανά μήνα
 
 EFKA = 13.867
-INSURANCE_CATEGORIES = [210, 252, 302, 363, 435, 566]
-OAEE_PENSION_CATEGORIES = [791, 892, 1003, 1163, 1353, 1697]
 INCOME_TAX_CATEGORIES = {
     10000: 9, 
     20000: 22, 
@@ -18,25 +16,29 @@ INCOME_TAX_CATEGORIES = {
     40000: 36,
     1000000: 44
 }
+INSURANCE_CATEGORIES = [210, 252, 302, 363, 435, 566]
 MAX_NATIONAL_PENSION = 384
-MAX_CONTRIBUTORY_PENSION_FACTOR = 56.7 # after 45 years of work
-
-category = int(input('1. Υπάλληλος\n2. Ελεύθερος επαγγελματίας\n3. Εταιρία\nΔιάλεξε: '))
-beforeTaxIncome = int(input('Μεικτά έσοδα: '))
+MAX_CONTRIBUTORY_PENSION_FACTOR_1 = 56.7 # after 45 years of work as an employee
+MAX_CONTRIBUTORY_PENSION_FACTOR_2 = 52.51 # after 45 years of work as an employer!
+GREEK_CATEGORIES = {
+    'employee': 'ΥΠΑΛΛΗΛΟΣ', 
+    'freelancer': 'ΕΛΕΥΘΕΡΟΣ ΕΠΑΓΓΕΛΜΑΤΙΑΣ', 
+    'company': 'ΕΤΑΙΡΙΑ'
+}
 
 # taxable income and pension before tax
-if category == 1:
-    insuranceContributions = min(beforeTaxIncome, 91000) * EFKA / 100 # 91000 = 6500 * 14 months
-    beforeTaxPension = MAX_NATIONAL_PENSION * 12 + beforeTaxIncome * MAX_CONTRIBUTORY_PENSION_FACTOR / 100
-elif category == 2 or category == 3:
-    INSURANCE_CATEGORIES = [item * 12 for item in INSURANCE_CATEGORIES]
-    expectedContributions = min(beforeTaxIncome, 91000) * EFKA / 100
-    distances = [abs(expectedContributions-i*12) for i in INSURANCE_CATEGORIES]
-    min_index = distances.index(min(distances))
-    insuranceContributions = INSURANCE_CATEGORIES[min_index]
-    beforeTaxPension = OAEE_PENSION_CATEGORIES[min_index] * 12
-
-taxableIncome = beforeTaxIncome - insuranceContributions
+def insurance_calc(category, beforeTaxIncome):
+    if category == 'employee':
+        insuranceContributions = min(beforeTaxIncome, 91000) * EFKA / 100 # 91000 = 6500 * 14 months
+        beforeTaxPension = MAX_NATIONAL_PENSION * 12 + beforeTaxIncome * MAX_CONTRIBUTORY_PENSION_FACTOR_1 / 100
+    elif category == 'freelancer' or category == 'company':
+        expectedContributions = min(beforeTaxIncome, 91000) * EFKA / 100
+        distances = [abs(expectedContributions-i*12) for i in INSURANCE_CATEGORIES]
+        min_index = distances.index(min(distances))
+        insuranceContributions = INSURANCE_CATEGORIES[min_index] * 12
+        beforeTaxPension = MAX_NATIONAL_PENSION * 12 + insuranceContributions / 0.2 * MAX_CONTRIBUTORY_PENSION_FACTOR_2 / 100
+    taxableIncome = beforeTaxIncome - insuranceContributions
+    return insuranceContributions, beforeTaxPension, taxableIncome
 
 # Income Tax
 def income_tax_calc(income):
@@ -50,26 +52,53 @@ def income_tax_calc(income):
         incomeTax += (High - Low) * Factor / 100
         Low = High
     return incomeTax
-incomeTax = income_tax_calc(taxableIncome)
-pensionTax = income_tax_calc(beforeTaxPension)
 
 # Tax Deduction
-if category == 1:
-    incomeTaxDeduction = max(0, 777 - 20 * floor((taxableIncome - 12000) / 1000))
-    pensionTaxDeduction = max(0, 777 - 20 * floor((beforeTaxPension - 12000) / 1000))
-elif category == 2 or category == 3:
-    incomeTaxDeduction = 0
-    pensionTaxDeduction = 0
+def tax_detuction_calc(category, taxableIncome, beforeTaxPension):    
+    if category == 'employee':
+        incomeTaxDeduction = max(0, 777 - 20 * floor((taxableIncome - 12000) / 1000))
+        pensionTaxDeduction = max(0, 777 - 20 * floor((beforeTaxPension - 12000) / 1000))
+    elif category == 'freelancer' or category == 'company':
+        incomeTaxDeduction = 0
+        pensionTaxDeduction = max(0, 777 - 20 * floor((beforeTaxPension - 12000) / 1000))
+    return incomeTaxDeduction, pensionTaxDeduction
 
-# After-tax income
-afterTaxIncome = taxableIncome - incomeTax + incomeTaxDeduction
-afterTaxPension = beforeTaxPension - pensionTax + pensionTaxDeduction
+def calculator(beforeTaxIncome, category):
+    # insurance
+    insuranceContributions, beforeTaxPension, taxableIncome = insurance_calc(category, beforeTaxIncome)
 
-print('\nΦορολογητέο εισόδημα =',taxableIncome)
-print('Φόρος εισοδήματος =',incomeTax)
-print('Έκπτωση φόρου =',incomeTaxDeduction)
-print('Καθαρό εισόδημα =',afterTaxIncome)
-print('\nΣύνταξη =',beforeTaxPension)
-print('Φόρος εισοδήματος  =',pensionTax)
-print('Έκπτωση φόρου =',pensionTaxDeduction)
-print('Καθαρό εισόδημα =',afterTaxPension)
+    # income tax
+    if category == 'company':
+        incomeTax = taxableIncome * 22 / 100
+    else: 
+        incomeTax = income_tax_calc(taxableIncome)
+    pensionTax = income_tax_calc(beforeTaxPension)
+
+    # tax detuction
+    incomeTaxDeduction, pensionTaxDeduction = tax_detuction_calc(category, taxableIncome, beforeTaxPension)
+
+    # After-tax income
+    afterTaxIncome = taxableIncome - incomeTax + incomeTaxDeduction
+    afterTaxPension = beforeTaxPension - pensionTax + pensionTaxDeduction
+
+    return insuranceContributions, beforeTaxPension, taxableIncome, incomeTax, pensionTax, incomeTaxDeduction, afterTaxIncome, pensionTaxDeduction, afterTaxPension
+
+def main():
+    beforeTaxIncome = int(input('Μεικτά έσοδα: '))
+
+    for category in ['employee','freelancer','company']:
+        insuranceContributions, beforeTaxPension, taxableIncome, incomeTax, pensionTax, incomeTaxDeduction, afterTaxIncome, pensionTaxDeduction, afterTaxPension = calculator(beforeTaxIncome, category)
+        print('\n---',GREEK_CATEGORIES[category])
+        print('Καθαρό εισόδημα =',int(afterTaxIncome))
+        print('Καθαρή σύνταξη =',int(afterTaxPension))
+        print('-- Ανάλυση εισοδήματος')
+        print('Ασφαλιστικές εισφορές =',int(insuranceContributions))
+        print('Φορολογητέο εισόδημα =',int(taxableIncome))
+        print('Φόρος εισοδήματος =',int(incomeTax))
+        print('Έκπτωση φόρου =',int(incomeTaxDeduction))
+        print('-- Ανάλυση σύνταξης')
+        print('Φόρος εισοδήματος =',int(pensionTax))
+        print('Έκπτωση φόρου =',int(pensionTaxDeduction))
+
+if __name__ == "__main__":
+    main()
